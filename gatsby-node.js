87f7cc5e8c9c0a9b4a3119a73eae2f559ea40881
Mark Sanders <mark@qcollective.com>
2024-09-19 15:29:21 -0400

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -90,8 +90,8 @@ async function indexContentInTypesense({
 // New function to get existing synonyms
 const getExistingSynonyms = async (client, collectionName) => {
   try {
-    const synonyms = await client.collections(collectionName).synonyms().retrieve();
-    return synonyms;
+    const response = await client.collections(collectionName).synonyms().retrieve();
+    return Object.values(response.synonyms || {});
   } catch (error) {
     console.warn(`Failed to retrieve synonyms from ${collectionName}:`, error);
     return [];
@@ -102,7 +102,9 @@ const getExistingSynonyms = async (client, collectionName) => {
 const upsertSynonyms = async (client, collectionName, synonyms) => {
   for (const synonym of synonyms) {
     try {
-      await client.collections(collectionName).synonyms().upsert(synonym.id, synonym);
+      await client.collections(collectionName).synonyms().upsert(synonym.id, {
+        synonyms: synonym.synonyms
+      });
     } catch (error) {
       console.warn(`Failed to upsert synonym ${synonym.id}:`, error);
     }
@@ -120,7 +122,8 @@ exports.onPostBuild = async (
     generateNewCollectionName = utils.generateNewCollectionName,
   }
 ) => {
-  reporter.verbose("[Typesense] Getting list of HTML files")
+  reporter.info("[Typesense] Starting onPostBuild process")
+  
   // backward compatibility
   rootDir = rootDir || publicDir
   const htmlFiles = await utils.getHTMLFilesRecursively(rootDir, rootDir, exclude)
@@ -130,29 +133,30 @@ exports.onPostBuild = async (
   const newCollectionSchema = { ...collectionSchema }
   newCollectionSchema.name = newCollectionName
 
+  reporter.info(`[Typesense] New collection name: ${newCollectionName}`)
+
   let oldCollectionName
   let existingSynonyms = []
-  
-  try {
-    oldCollectionName = (
-      await typesense.aliases(collectionSchema.name).retrieve()
-    )["collection_name"]
-    reporter.verbose(`[Typesense] Old collection name was ${oldCollectionName}`)
 
-    // Get existing synonyms before creating the new collection
-    const existingSynonyms = await getExistingSynonyms(typesense, oldCollectionName);
-    reporter.verbose(`[Typesense] Retrieved ${existingSynonyms.length} synonyms from old collection`)
+  try {
+    const aliasInfo = await typesense.aliases(collectionSchema.name).retrieve()
+    oldCollectionName = aliasInfo["collection_name"]
+    reporter.info(`[Typesense] Old collection name: ${oldCollectionName}`)
+
+    if (oldCollectionName) {
+      reporter.info(`[Typesense] Retrieving synonyms from old collection: ${oldCollectionName}`)
+      existingSynonyms = await getExistingSynonyms(typesense, oldCollectionName)
+      reporter.info(`[Typesense] Retrieved ${existingSynonyms.length} synonyms from old collection`)
+    }
   } catch (error) {
-    reporter.verbose(`[Typesense] No old collection found, proceeding without synonyms`)
+    reporter.warn(`[Typesense] Error retrieving old collection or synonyms: ${error.message}`)
   }
 
   try {
-    reporter.verbose(`[Typesense] Creating collection ${newCollectionName}`)
+    reporter.info(`[Typesense] Creating new collection: ${newCollectionName}`)
     await typesense.collections().create(newCollectionSchema)
   } catch (error) {
-    reporter.panic(
-      `[Typesense] Could not create collection ${newCollectionName}: ${error}`
-    )
+    reporter.panic(`[Typesense] Could not create collection ${newCollectionName}: ${error.message}`)
   }
 
   for (const file of htmlFiles) {
@@ -168,41 +172,39 @@ exports.onPostBuild = async (
     })
   }
 
-  // Upsert synonyms after indexing is complete
-  if (existingSynonyms && existingSynonyms.length > 0) {
-    reporter.verbose(`[Typesense] Upserting ${existingSynonyms.length} synonyms to new collection`)
-    await upsertSynonyms(typesense, newCollectionName, existingSynonyms);
+  if (existingSynonyms.length > 0) {
+    reporter.info(`[Typesense] Upserting ${existingSynonyms.length} synonyms to new collection`)
+    try {
+      await upsertSynonyms(typesense, newCollectionName, existingSynonyms)
+      reporter.info(`[Typesense] Successfully upserted synonyms to new collection`)
+    } catch (error) {
+      reporter.error(`[Typesense] Error upserting synonyms: ${error.message}`)
+    }
+  } else {
+    reporter.info(`[Typesense] No synonyms to upsert`)
   }
 
   try {
-    reporter.verbose(
-      `[Typesense] Upserting alias ${collectionSchema.name} -> ${newCollectionName}`
-    )
+    reporter.info(`[Typesense] Upserting alias ${collectionSchema.name} -> ${newCollectionName}`)
     await typesense
       .aliases()
       .upsert(collectionSchema.name, { collection_name: newCollectionName })
-
-    reporter.info(
-      `[Typesense] Content indexed to "${collectionSchema.name}" [${newCollectionName}]`
-    )
+    reporter.info(`[Typesense] Successfully upserted alias`)
   } catch (error) {
-    reporter.error(
-      `[Typesense] Could not upsert alias ${collectionSchema.name} -> ${newCollectionName}: ${error}`
-    )
+    reporter.error(`[Typesense] Could not upsert alias ${collectionSchema.name} -> ${newCollectionName}: ${error.message}`)
   }
 
-  try {
-    if (oldCollectionName) {
-      reporter.verbose(
-        `[Typesense] Deleting old collection ${oldCollectionName}`
-      )
+  if (oldCollectionName) {
+    try {
+      reporter.info(`[Typesense] Deleting old collection ${oldCollectionName}`)
       await typesense.collections(oldCollectionName).delete()
+      reporter.info(`[Typesense] Successfully deleted old collection`)
+    } catch (error) {
+      reporter.error(`[Typesense] Could not delete old collection ${oldCollectionName}: ${error.message}`)
     }
-  } catch (error) {
-    reporter.error(
-      `[Typesense] Could not delete old collection ${oldCollectionName}: ${error}`
-    )
   }
+
+  reporter.info(`[Typesense] Content indexed to "${collectionSchema.name}" [${newCollectionName}]`)
 }
 
 exports.onPreInit = ({ reporter }) =>
